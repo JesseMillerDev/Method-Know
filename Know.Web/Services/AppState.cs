b11ac6cef3a1c7bc86ad70f647b074638b6a6b49
Jesse Miller <jesse.miller@method.com>
2025-12-17 14:41:31 -0800

    feat: Introduce 'Share Resource' button with associated app state event and refactor header UI with updated CSS animations and utility classes.

--- Know.Web/Services/AppState.cs
+++ Know.Web/Services/AppState.cs
@@ -7,11 +7,17 @@ namespace Know.Web.Services
         public string SearchQuery { get; private set; } = "";
 
         public event Action<string>? OnSearchChanged;
+        public event Action? OnAddResourceRequested;
 
         public void SetSearchQuery(string query)
         {
             SearchQuery = query;
             OnSearchChanged?.Invoke(SearchQuery);
+        }
+
+        public void RequestAddResource()
+        {
+            OnAddResourceRequested?.Invoke();
         }
     }
 }
