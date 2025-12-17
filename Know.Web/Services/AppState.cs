e392b5ea981216b49b99c64abc1ad5e6386484f9
Jesse Miller <jesse.miller@method.com>
2025-12-17 15:51:54 -0800

    feat: Centralize resource modal in MainLayout, removing it from pages and adding AppState events for global control.

--- Know.Web/Services/AppState.cs
+++ Know.Web/Services/AppState.cs
@@ -8,6 +8,10 @@ namespace Know.Web.Services
 
         public event Action<string>? OnSearchChanged;
         public event Action? OnAddResourceRequested;
+        public event Action<Know.Shared.Models.Article>? OnViewResourceRequested;
+        public event Action<Know.Shared.Models.Article>? OnArticleCreated;
+        public event Action<Know.Shared.Models.Article>? OnArticleUpdated;
+        public event Action<int>? OnArticleDeleted;
 
         public void SetSearchQuery(string query)
         {
@@ -18,6 +22,26 @@ namespace Know.Web.Services
         public void RequestAddResource()
         {
             OnAddResourceRequested?.Invoke();
+        }
+
+        public void RequestViewResource(Know.Shared.Models.Article article)
+        {
+            OnViewResourceRequested?.Invoke(article);
+        }
+
+        public void NotifyArticleCreated(Know.Shared.Models.Article article)
+        {
+            OnArticleCreated?.Invoke(article);
+        }
+
+        public void NotifyArticleUpdated(Know.Shared.Models.Article article)
+        {
+            OnArticleUpdated?.Invoke(article);
+        }
+
+        public void NotifyArticleDeleted(int articleId)
+        {
+            OnArticleDeleted?.Invoke(articleId);
         }
     }
 }
